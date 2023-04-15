d3a80026a0c0cad475194f53b8cfeb371646c15d
thistechguy <86990020+thistechguy@users.noreply.github.com>
2023-04-15 11:21:57 -0400

    Update Fedora_Setup.sh
    Added Microsoft Fonts

--- Fedora_Setup.sh
+++ Fedora_Setup.sh
@@ -15,6 +15,10 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo dnf install microsoft-edge-stable
 
+#Install Microsoft Fonts 
+sudo dnf install curl cabextract xorg-x11-font-utils fontconfig
+sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
 #Install VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
